c983561f86314cef69840523a43022ce0fe3c43b
Joel <joel.roca.martinez@hotmail.com>
2021-01-04 19:15:30 +0100

    Home Page done exept background image

--- front-end/src/components/MyNavBar.js
+++ front-end/src/components/MyNavBar.js
@@ -8,7 +8,7 @@ function MyNavBar() {
         <div id="navbar">
             <h5 className="profile">Profile</h5>
             <h5 className="notifications">Notifications</h5>
-            <h3 className="brand">Tibia Hunting Records</h3>
+            <h2 className="brand">Tibia Hunting Records</h2>
             <h5 className="newRecord">New Record</h5>
             <h5 className="huntingRecords">Hunting Records</h5>
         </div>
