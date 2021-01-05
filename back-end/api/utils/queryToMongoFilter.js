765f6aa7d99c35dba72b7b8c1a948ae6794223cd
Gerard <gcsaperas@gmai.com>
2021-01-05 07:47:05 +0100

    set up queryToMongoFilter util

--- back-end/api/utils/queryToMongoFilter.js
+++ back-end/api/utils/queryToMongoFilter.js
@@ -1,5 +1,41 @@
-function queryToMongoFilter() {
-    return null;
+function queryToMongoFilter(queryParams) {
+    var andFilter = []
+    let filter = {
+        conditions: {}
+    };
+
+    if (Array.isArray(queryParams.fields) && Array.isArray(queryParams.values)) {
+        for (var i = 0; i < queryParams.fields.length; i++) {
+            if (queryParams.values[i]) {
+                let fieldName = queryParams.fields[i]
+                let value = { $regex: queryParams.values[i], $options: 'i' }
+                andFilter.push({
+                    [fieldName]:value
+                })
+            }
+        }
+    }
+
+    else if (queryParams.fields && queryParams.values) {
+        let fieldName = queryParams.fields
+        let value = { $regex: queryParams.values, $options: 'i' }
+        andFilter.push({
+            [fieldName]:value
+        })
+    }
+
+    if (andFilter.length > 0){
+        filter["conditions"]["$and"] = andFilter
+    } else {
+        filter["conditions"]["$and"] = [];
+    }
+
+    if (queryParams.offset && queryParams.limit) {
+        filter.offset = parseInt(queryParams.offset);
+        filter.limit = parseInt(queryParams.limit);
+    }
+    
+    return filter;
 }
 
 module.exports = {
