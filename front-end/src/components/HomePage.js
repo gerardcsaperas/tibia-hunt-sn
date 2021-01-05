3ad64549e76bf79a578c6e994b42e0aef2e21685
Gerard <gcsaperas@gmai.com>
2021-01-05 09:50:09 +0100

    remove single-use content, replace with reusable

--- front-end/src/components/HomePage.js
+++ front-end/src/components/HomePage.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Button from './custom/Button/Button';
+import ContentBox from './custom/ContentBox/ContentBox';
 import "./styles/HomePage.css";
 
 
 
 function HomePage() {
+
+  const content = (
+    <Fragment>
+      <div className="text">
+        <p className="lore">Dear tibians,</p>
+        <p className="lore">Welcome to Tibia Hunting Records, a platform that has been created with a clear idea: Solve the where to go problem.</p>
+        <p className="lore">We want to invite you to join this wonderful community and together build the biggest data base for hunting spots ever!</p>    
+        <p className="lore">Join us in this exciting quest and share your best achievents in this wonderful game called tibia!</p>
+      </div>
+      <div className="buttons__box">
+        <button className="button">Create an account</button>
+        <button className="button">Hunting Records</button>
+      </div>
+    </Fragment>
+  )
+
     return (
-      <div id="home">
-            <div className="home-page__title">
-                <h3>Tibia Hunting Records</h3>   
-            </div>
-              <div className="home-page__box">
-                <div className="content">
-                  <div className="text">
-                    <p className="lore">Dear tibians,</p>
-                    <p className="lore">Welcome to Tibia Hunting Records, a platform that has been created with a clear idea: Solve the where to go problem.</p>
-                    <p className="lore">We want to invite you to join this wonderful community and together build the biggest data base for hunting spots ever!</p>    
-                    <p className="lore">Join us in this exciting quest and share your best achievents in this wonderful game called tibia!</p>
-                  </div>
-                  <div className="buttons">
-                    <button className="button">Create an account</button>
-                    <button className="button">Hunting Records</button>
-                  </div>    
-                </div>
-            </div>
-        </div>
+    <ContentBox
+      height="480px"
+      width="370px"
+      title="Tibia Hunting Records"
+      content={content}
+    ></ContentBox>
     )
 }
 
