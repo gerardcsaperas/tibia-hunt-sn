463c25559896a193f51d154a254451960402011a
Gerard <gcsaperas@gmai.com>
2021-01-05 07:46:46 +0100

    remove notification routes

--- back-end/api/routes/user.js
+++ back-end/api/routes/user.js
@@ -7,8 +7,6 @@ router.get('/user', auth, userController.findMe);
 router.get('/user/:id', auth, userController.findById);
 router.post('/user', userController.create);
 router.put('/user', auth, userController.update);
-router.put('/user/notifications', auth, userController.updateNotifications);
-router.get('/user/notifications', auth, userController.findNotifications);
 router.delete('/user', auth, userController.remove);
 
 module.exports = router;
