f964dbda035e5d8dceaa6786bbe13c978effce4a
Gerard <gcsaperas@gmai.com>
2021-01-05 08:49:19 +0100

    make responsive, center title

--- front-end/src/components/MyNavBar.js
+++ front-end/src/components/MyNavBar.js
@@ -6,11 +6,15 @@ import "./styles/MyNavBar.css";
 function MyNavBar() {
     return (
         <div id="navbar">
-            <h5 className="profile">Profile</h5>
-            <h5 className="notifications">Notifications</h5>
+            <div id="navbar__left-box">
+                <h5 className="profile">Profile</h5>
+                <h5 className="notifications">Notifications</h5>
+            </div>
             <h2 className="brand">Tibia Hunting Records</h2>
-            <h5 className="newRecord">New Record</h5>
-            <h5 className="huntingRecords">Hunting Records</h5>
+            <div id="navbar__right-box">
+                <h5 className="newRecord">New Record</h5>
+                <h5 className="huntingRecords">Hunting Records</h5>
+            </div>
         </div>
     )
 }
