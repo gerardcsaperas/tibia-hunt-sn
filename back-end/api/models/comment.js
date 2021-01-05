8fd789dee6fb775bcbeb43d1c32e63cc9dee8029
Gerard <gcsaperas@gmai.com>
2021-01-05 07:48:16 +0100

    comment schema

---
+++ back-end/api/models/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, { timestamps: true })
+
+module.exports = commentSchema;
