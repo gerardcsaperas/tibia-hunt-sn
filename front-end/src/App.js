cf03a79ada42116b4b617a26a2d1551d293481b4
Gerard <gcsaperas@gmai.com>
2021-01-06 08:52:46 +0100

    redux store, router, effect, state

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import HomePage from "./components/HomePage";
-import MyNavBar from "./components/MyNavBar";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+// Own Components
+import HomePage from './components/HomePage';
+import MyNavBar from './components/MyNavBar';
+import Profile from './components/user/Profile';
 
 
+// Utils
+import isTokenValid from './utils/isTokenValid';
 
+// State Store
+import { useSelector, useDispatch } from 'react-redux'
+import {
+	setUser,
+	setToken,
+	setUid,
+	authenticate,
+	selectUser
+} from './components/user/userSlice'
+import {
+	setWidth,
+	setHeight
+} from './components/layout/viewportSlice'
 
 function App() {
+
+  const authenticated = true //useSelector(selectUser).authenticated;
+	const dispatch = useDispatch()
+
+	// Check if user is already authenticated.
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem('user'));
+	console.log(process.env.NODE_ENV);
+	console.log('authenticated', authenticated)
+	
+    if (process.env.NODE_ENV == "development") {
+      dispatch(authenticate());
+    }
+		// If token is stored in local storage, see if token is valid
+		if (user.token) {
+			let isAuthenticated = async () => await isTokenValid(user.token);
+			// If token is valid, user is authenticated
+			// set all values for user
+			if (isAuthenticated) {
+				dispatch(authenticate());
+				dispatch(setUser(user.user.name));
+				dispatch(setUid(user.user._id));
+				dispatch(setToken(user.token));
+			}
+		}
+	}, [])
+
+	// Window resize listener to make custom responsive components
+	useEffect(() => {
+        updateWindowDimensions()
+        window.addEventListener('resize', updateWindowDimensions);
+    }, [])
+
+    const updateWindowDimensions = () => {
+		dispatch(setWidth(window.innerWidth));
+		dispatch(setHeight(window.innerHeight));
+    }
+
   return (
-    <div className="App">
-      <MyNavBar/>
-      <HomePage/>
-    </div>
+    <Router>
+      <Route>
+				<MyNavBar props={authenticated}/>
+			</Route>
+      <Switch>
+        <Route exact path="/" >
+					{ authenticated ? <Redirect to="/profile" /> : <HomePage /> }
+		</Route>
+		<Route exact path="/profile" >
+					{ authenticated ? <Profile /> : <Redirect to="/login" /> }
+		</Route>
+      </Switch>
+    </Router>
   );
 }
 
