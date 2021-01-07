3ba3e28b0587599d6111d4f49928c7f65629ddb0
gcsaperas <gcsaperas@konodrac.com>
2021-01-08 00:58:54 +0100

    do not double-save user

--- back-end/api/controllers/user.js
+++ back-end/api/controllers/user.js
@@ -53,8 +53,6 @@ async function create(req, res) {
 
         const token = await user.generateAuthToken();
 
-        await user.save()
-
         res.status(201).send({ user, token });
         
 	} catch (e) {
