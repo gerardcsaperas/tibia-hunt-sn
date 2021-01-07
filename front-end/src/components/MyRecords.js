a709afe89e6da00c31afc5ff5b59e45f9590ea52
Joel <joel.roca.martinez@hotmail.com>
2021-01-07 13:30:38 +0100

    added the logic behind the records list

--- front-end/src/components/MyRecords.js
+++ front-end/src/components/MyRecords.js
@@ -6,7 +6,20 @@ import "./styles/MyRecords.css";
 
 
 
-function HomePage() {
+function MyRecords() {
+
+    // const handleSearch = (e) => {
+    //     let search = e.currentTarget.value.toLowerCase();
+    //     let cloneChallenges = this.state.challenges.filter((item) => {
+    //       return (
+    //         item.title.toLowerCase().includes(search) ||
+    //         item.description.toLowerCase().includes(search)
+    //       );
+    //     });
+    //     this.setState({
+    //       filteredChallenges: cloneChallenges,
+    //     });
+    //   };
 
   const content = (
     <Fragment>
@@ -60,27 +73,66 @@ function HomePage() {
               </label>
             </form>
         </div>
+{/* 
+        <ol className="list">
+          {this.state.huntingRecord.map((record) => {
+            const {picture, location, city, characterName, level, vocation, expRatio, expH, profitH, likesCount, dislikesCount, commentsCount } = user;
+            return (
+              <Link to={`/hunting-record/${_id}`}>
+                  <li className="recordSummary">
+                    {username} {points} points
+                  </li> 
+              </Link>
 
-        <ol className="recordSummary">
-            <div className="lootPicContainer">
-                <img className="lootPicture" src="/images/tibia-background-artwork.jpg" alt="defaultImg"></img>
-            </div>
+            <Link to={`/hunting-record/${_id}`}>
+                <li className="recordSummary">
+                    <div className="lootPicContainer">
+                        {picture}
+                    </div>
 
-            <div className="basicInfo" >
-                <h1 className="location">Demons, Edron</h1>
-                <div className="details">
-                    <p className="info">Lunatek, 250 ED</p>
-                    <p className="info">Exp/h: 1.500.000/h</p>
-                    <p className="info">Exp Ratio: 150%</p>
-                    <p className="info">Profit/h: 250.000/h</p>
+                    <div className="basicInfo" >
+                        <h1 className="location">{location}, {city}</h1>
+                        <div className="details">
+                            <p className="info">{characterName}, {level} {vocation}</p>
+                            <p className="info">Exp/h: {expH}</p>
+                            <p className="info">Exp Ratio: {expRatio}</p>
+                            <p className="info">Profit/h: {profitH}</p>
+                        </div>
+
+                        <div className="socialMedia">
+                            <i class="far fa-thumbs-up">{likesCount}</i>
+                            <i class="far fa-thumbs-down">{dislikesCount}</i>
+                            <i class="fas fa-comment">{commentsCount}</i>
+                        </div>
+                    </div>
+                </li>
+            </Link>
+            );
+          })}
+        </ol> */}
+
+        <ol>
+            <li className="recordSummary">
+                <div className="lootPicContainer">
+                    <img className="lootPicture" src="/images/tibia-background-artwork.jpg" alt="defaultImg"></img>
                 </div>
-            <div className="socialMedia">
-                <i class="far fa-thumbs-up"></i>
-                <i class="far fa-thumbs-down"></i>
-                <i class="fas fa-comment"></i>
-            </div>
-               
-            </div>
+
+                <div className="basicInfo" >
+                    <h1 className="location">Demons, Edron</h1>
+                    <div className="details">
+                        <p className="info">Lunatek, 250 ED</p>
+                        <p className="info">Exp/h: 1.500.000/h</p>
+                        <p className="info">Exp Ratio: 150%</p>
+                        <p className="info">Profit/h: 250.000/h</p>
+                    </div>
+                <div className="socialMedia">
+                    <i class="far fa-thumbs-up"></i>
+                    <i class="far fa-thumbs-down"></i>
+                    <i class="fas fa-comment"></i>
+                </div>
+                
+                </div>
+            </li>
         </ol>
     </Fragment>
   )
@@ -95,4 +147,4 @@ function HomePage() {
     )
 }
 
-export default HomePage
+export default MyRecords
