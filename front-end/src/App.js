e01f7e11be7df2281e35b6f09cb1f8937eebc6d7
Joel <joel.roca.martinez@hotmail.com>
2021-01-07 12:20:18 +0100

    added the grafics of the records list

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import './App.css';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 // Components
@@ -28,3 +33,4 @@ function App() {
 }
 
 export default App;
+
--- front-end/src/components/MyRecords.js
+++ front-end/src/components/MyRecords.js
@@ -5,14 +5,15 @@ import "./styles/MyRecords.css";
 
 
 
+
 function HomePage() {
 
   const content = (
     <Fragment>
-      <div className="filters">
-      <h1>Filters</h1>
-      <img className="loupe" src="/images/Loupe.gif" alt="Loupe" />
-      <form className="grill">
+        <div className="filters">
+        <h1>Filters</h1>
+          <img className="loupe" src="/images/Loupe.gif" alt="Loupe" />
+            <form className="grill">
               <label className="dividers">
                   Level:
                   <select className="selector"/>
@@ -57,9 +58,30 @@ function HomePage() {
                   Difficulty:
                   <select className="selector"/>
               </label>
-          </form>
+            </form>
+        </div>
 
-      </div>
+        <ol className="recordSummary">
+            <div className="lootPicContainer">
+                <img className="lootPicture" src="/images/tibia-background-artwork.jpg" alt="defaultImg"></img>
+            </div>
+
+            <div className="basicInfo" >
+                <h1 className="location">Demons, Edron</h1>
+                <div className="details">
+                    <p className="info">Lunatek, 250 ED</p>
+                    <p className="info">Exp/h: 1.500.000/h</p>
+                    <p className="info">Exp Ratio: 150%</p>
+                    <p className="info">Profit/h: 250.000/h</p>
+                </div>
+            <div className="socialMedia">
+                <i class="far fa-thumbs-up"></i>
+                <i class="far fa-thumbs-down"></i>
+                <i class="fas fa-comment"></i>
+            </div>
+               
+            </div>
+        </ol>
     </Fragment>
   )
 
