5f3e999eff6b2882bb7220e07818c23fe32fe6b7
gcsaperas <gcsaperas@konodrac.com>
2021-01-10 09:25:32 +0100

    dinamically set height based on innermost height

--- front-end/src/components/custom/ContentBox/ContentBox.jsx
+++ front-end/src/components/custom/ContentBox/ContentBox.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import ScaleLoader from "react-spinners/ScaleLoader";
 import './ContentBox.scss';
 
 // Redux Store
@@ -7,21 +8,34 @@ import { selectViewport } from '../../layout/viewportSlice'
 
 function ContentBox(props) {  
 
+    // Variable to continuously store viewport dimensions to redux
     const windowDimensions = useSelector(selectViewport);
+    
+    /*
+    Minimum height is defined at 500px.
+    The box automatically detects height in order to style
+    other elements.
+    */
+   const contentBox = useRef();
+    const [ frameHeight, setFrameHeight ] = useState('auto');
 
-    const height = parseInt(props.height);
-    const frameHeight = height - 40;
-    const contentBoxHeight = frameHeight - 8;
+    useEffect(() => {
+        if (contentBox.current) {
+            setFrameHeight(contentBox.current.clientHeight + 8);
+        }
+    }, [contentBox])
+
+    // Width can be hard coded just fine
     const width = parseInt(props.width);
     const contentBoxWidth = width - 8
 
     const desktopComponent = (
-        <div className="ContentBox" style={{height: height, width: width}}>
+        <div className="ContentBox" style={{width: width}}>
             <div className="ContentBox__title">
                 <h3>{props.title}</h3>
             </div>
             <div className="ContentBox__frame" style={{height: frameHeight, width: width}}>
-                <div className="ContentBox__content" style={{height: contentBoxHeight, width: contentBoxWidth}}>{props.content}</div>
+                <div className="ContentBox__content" ref={contentBox} style={{minHeight: 430, width: contentBoxWidth}}>{props.content}</div>
             </div>
         </div>
     )
