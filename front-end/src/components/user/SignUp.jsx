6441620fa3dcfec902dc24ac99dd2f91353c8194
gcsaperas <gcsaperas@konodrac.com>
2021-01-10 11:51:20 +0100

    Signup moved to /user

--- front-end/src/components/user/SignUp.jsx
+++ front-end/src/components/user/SignUp.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import axios from "axios";
-import { API_URL } from "../config";
+import { API_URL } from "../../config";
 import {Link} from 'react-router-dom';
-import ContentBox from './custom/ContentBox/ContentBox';
-import "./styles/SignUp.css";
-import { setUser } from './user/userSlice';
+import ContentBox from '../custom/ContentBox/ContentBox';
+import FormBox from '../custom/FormBox/FormBox';
+import { setUser } from './userSlice';
 
 
 //HERE
@@ -69,40 +69,39 @@ function SignUp() {
     }
   };
 
+  const form = (
+    <form className="signup-form">
+      <div className="form-input-row">
+        <label>User Name:</label>
+        <input type="text" name="username" onChange={(e) => updateUsername(e)} autoComplete="off"/>
+        {usernameError ? <p style={{color: "red"}}>Username already in use</p> : null }
+      </div>
+      <div className="form-input-row">
+        <label>Email Adress:</label>
+        <input type="email" name="email" onChange={(e) => updateEmail(e)} autoComplete="off"/>
+        {emailError ? <p style={{color: "red"}}>Email already in use</p> : null }
+      </div>   
+      <div className="form-input-row">
+        <label>Password:</label>
+        <input type="password" name="password" onChange={(e) => updatePassword(e)} autoComplete="off"/>
+        {passwordError ? <p style={{color: "red"}}>Password must be, at least, 8 characters long</p> : null }
+      </div>
+      <p>Already a member? <Link to="/login"> Login</Link></p>
+    </form>
+  )
+
   const content = (
     <Fragment>
-    <div className="signUp">
-          <form id="signup-form">
-            <div className="form-input-row">
-              <label className="dividers">User Name:</label>
-              <input type="text" name="username" onChange={(e) => updateUsername(e)} autoComplete="off"/>
-              {usernameError ? <p style={{color: "red"}}>Username already in use</p> : null }
-            </div>
-            <div className="form-input-row">
-              <label className="dividers"> Email Adress:</label>
-              <input type="email" name="email" onChange={(e) => updateEmail(e)} autoComplete="off"/>
-              {emailError ? <p style={{color: "red"}}>Email already in use</p> : null }
-            </div>   
-            <div className="form-input-row">
-              <label className="dividers"> Password:</label>
-              <input type="password" name="password" onChange={(e) => updatePassword(e)} autoComplete="off"/>
-              {passwordError ? <p style={{color: "red"}}>Password must be, at least, 8 characters long</p> : null }
-            </div>
-          </form>
-        <p className="toLogin">Already a member? <Link to="/login"> Login</Link></p>
-    </div>
-
-    <div className="buttons__box">
-      <button className="button" onClick={handleSignUp}>Sign Up</button>
-    </div>
-  </Fragment>
-    
+      <FormBox form={form} />
+      <div className="buttons__box absolute-bot">
+        <button className="button" onClick={handleSignUp}>Sign Up</button>
+      </div>
+    </Fragment> 
   )
 
     return (
     <ContentBox
-      height="480px"
-      width="370px"
+      width="370"
       title="Sign Up"
       content={content}
     ></ContentBox>
