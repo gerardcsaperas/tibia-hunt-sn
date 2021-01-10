aa0d287f99d3b0854d95c07ec4903b6047f38906
gcsaperas <gcsaperas@konodrac.com>
2021-01-10 11:51:08 +0100

    MyRecords moved to /huntingRecords

---
+++ front-end/src/components/huntingRecords/MyRecords.jsx
@@ -0,0 +1,149 @@
+import React, { Fragment } from 'react';
+import ContentBox from '../custom/ContentBox/ContentBox';
+import "./MyRecords.css";
+
+
+
+
+function MyRecords() {
+
+    // const handleSearch = (e) => {
+    //     let search = e.currentTarget.value.toLowerCase();
+    //     let cloneChallenges = this.state.challenges.filter((item) => {
+    //       return (
+    //         item.title.toLowerCase().includes(search) ||
+    //         item.description.toLowerCase().includes(search)
+    //       );
+    //     });
+    //     this.setState({
+    //       filteredChallenges: cloneChallenges,
+    //     });
+    //   };
+
+  const content = (
+    <Fragment>
+        <div className="filters">
+        <h1>Filters</h1>
+          <img className="loupe" src="/images/Loupe.gif" alt="Loupe" />
+            <form className="grill">
+              <label className="dividers">
+                  Level:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Vocation:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Exp Ratio:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Exp/h:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  City:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Special Event:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Profit/h:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Team:
+                  <select className="selector"/>
+              </label>
+
+              <label className="dividers">
+                  Difficulty:
+                  <select className="selector"/>
+              </label>
+            </form>
+        </div>
+{/* 
+        <ol className="list">
+          {this.state.huntingRecord.map((record) => {
+            const {picture, location, city, characterName, level, vocation, expRatio, expH, profitH, likesCount, dislikesCount, commentsCount } = user;
+            return (
+              <Link to={`/hunting-record/${_id}`}>
+                  <li className="recordSummary">
+                    {username} {points} points
+                  </li> 
+              </Link>
+
+            <Link to={`/hunting-record/${_id}`}>
+                <li className="recordSummary">
+                    <div className="lootPicContainer">
+                        {picture}
+                    </div>
+
+                    <div className="basicInfo" >
+                        <h1 className="location">{location}, {city}</h1>
+                        <div className="details">
+                            <p className="info">{characterName}, {level} {vocation}</p>
+                            <p className="info">Exp/h: {expH}</p>
+                            <p className="info">Exp Ratio: {expRatio}</p>
+                            <p className="info">Profit/h: {profitH}</p>
+                        </div>
+
+                        <div className="socialMedia">
+                            <i class="far fa-thumbs-up">{likesCount}</i>
+                            <i class="far fa-thumbs-down">{dislikesCount}</i>
+                            <i class="fas fa-comment">{commentsCount}</i>
+                        </div>
+                    </div>
+                </li>
+            </Link>
+            );
+          })}
+        </ol> */}
+
+        <ol>
+            <li className="recordSummary">
+                <div className="lootPicContainer">
+                    <img className="lootPicture" src="/images/tibia-background-artwork.jpg" alt="defaultImg"></img>
+                </div>
+
+                <div className="basicInfo" >
+                    <h1 className="location">Demons, Edron</h1>
+                    <div className="details">
+                        <p className="charInfo">Lunatek, 250 ED</p>
+                        <p className="info">Exp/h: 1.500.000/h</p>
+                        <p className="expRatio">Exp Ratio: 150%</p>
+                        <p className="info">Profit/h: 250.000/h</p>
+                    </div>
+                <div className="socialMedia">
+                    <i class="far fa-thumbs-up"></i>
+                    <i class="far fa-thumbs-down"></i>
+                    <i class="fas fa-comment"></i>
+                </div>
+                
+                </div>
+            </li>
+        </ol>
+    </Fragment>
+  )
+
+    return (
+    <ContentBox
+      height="700px"
+      width="980px"
+      title="My Records"
+      content={content}
+    ></ContentBox>
+    )
+}
+
+export default MyRecords
