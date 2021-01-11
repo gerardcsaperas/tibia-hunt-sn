5a96faf051d87f8a9b3c306892bee381bcf7d39f
gcsaperas <gcsaperas@konodrac.com>
2021-01-11 09:13:01 +0100

    content box height based on content and padding...

--- front-end/src/components/custom/ContentBox/ContentBox.jsx
+++ front-end/src/components/custom/ContentBox/ContentBox.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import ScaleLoader from "react-spinners/ScaleLoader";
 import './ContentBox.scss';
 
 // Redux Store
@@ -10,32 +9,14 @@ function ContentBox(props) {
 
     // Variable to continuously store viewport dimensions to redux
     const windowDimensions = useSelector(selectViewport);
-    
-    /*
-    Minimum height is defined at 500px.
-    The box automatically detects height in order to style
-    other elements.
-    */
-   const contentBox = useRef();
-    const [ frameHeight, setFrameHeight ] = useState('auto');
-
-    useEffect(() => {
-        if (contentBox.current) {
-            setFrameHeight(contentBox.current.clientHeight + 8);
-        }
-    }, [contentBox])
-
-    // Width can be hard coded just fine
-    const width = parseInt(props.width);
-    const contentBoxWidth = width - 8
 
     const desktopComponent = (
-        <div className="ContentBox" style={{width: width}}>
+        <div className="ContentBox" style={{width: parseInt(props.width)}}>
             <div className="ContentBox__title">
                 <h3>{props.title}</h3>
             </div>
-            <div className="ContentBox__frame" style={{height: frameHeight, width: width}}>
-                <div className="ContentBox__content" ref={contentBox} style={{minHeight: 430, width: contentBoxWidth}}>{props.content}</div>
+            <div className="ContentBox__frame">
+                <div className="ContentBox__content">{props.content}</div>
             </div>
         </div>
     )
