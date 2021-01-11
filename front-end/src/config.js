ca96d929d347d2b8934bd6cb23f89836ffe1b118
gcsaperas <gcsaperas@konodrac.com>
2021-01-11 09:12:14 +0100

    store vocations

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,1 +1,2 @@
 export const API_URL = process.env.API_URL || 'http://localhost:3000/TibiaHuntingRecords_Api'
+export const vocations = ['Druid', 'ED', 'Knight', 'EK', 'Paladin', 'RP', 'Sorcerer', 'MS']
