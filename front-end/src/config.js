3763086b48648c257a98c185753b6cc358a21ccb
Gerard <gcsaperas@gmai.com>
2021-01-13 23:16:16 +0100

    5001, not 3000

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || 'http://localhost:3000/TibiaHuntingRecords_Api'
+export const API_URL = process.env.API_URL || 'http://localhost:5001/TibiaHuntingRecords_Api'
 export const vocations = ['Druid', 'ED', 'Knight', 'EK', 'Paladin', 'RP', 'Sorcerer', 'MS']
