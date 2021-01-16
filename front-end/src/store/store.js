1c3ebf0fd0929f316a0738e0466bc8821378f841
Gerard C. Saperas <gcsaperas@konodrac.com>
2021-01-16 21:16:45 +0100

    add charactersReducer

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../components/user/userSlice'
+import charactersReducer from '../components/character/characterSlice'
 import viewportReducer from '../components/layout/viewportSlice'
 
 export default configureStore({
   reducer: {
     viewport: viewportReducer,
-    user: userReducer
+    user: userReducer,
+    characters: charactersReducer
   }
 })
