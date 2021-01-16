2e6a9ddc32bae8473af0e6f7b394f3cdbfe0e555
Gerard C. Saperas <gcsaperas@konodrac.com>
2021-01-16 21:17:54 +0100

    login route

--- back-end/api/routes/user.js
+++ back-end/api/routes/user.js
@@ -7,6 +7,7 @@ const router = new express.Router();
 router.get('/user', auth, userController.findMe);
 router.get('/user/:id', auth, userController.findById);
 router.post('/user', userController.create);
+router.post('/user/login', userController.login);
 router.put('/user', auth, updateUserAuth, userController.update);
 router.delete('/user', auth, userController.remove);
 
