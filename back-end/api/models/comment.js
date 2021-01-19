fb8de837822de5da422b1fecd8378bd1924e6de0
Gerard C. Saperas <gcsaperas@konodrac.com>
2021-01-19 12:38:07 +0100

    patchLikes and dislikes methods

--- back-end/api/models/comment.js
+++ back-end/api/models/comment.js
@@ -20,4 +20,44 @@ const commentSchema = new Schema({
     }]
 }, { timestamps: true })
 
-module.exports = commentSchema;
+commentSchema.methods.patchLikes = async function(uid) {
+	const huntingRecord = this;
+    
+    let dislikeIndex = huntingRecord.dislikes.indexOf(uid);
+    if (dislikeIndex !== -1) {
+        huntingRecord.dislikes.splice(dislikeIndex, 1);
+    }
+
+    let likeIndex = huntingRecord.likes.indexOf(uid);
+    if ( likeIndex === -1) {
+        huntingRecord.likes.push(uid)
+    } else {
+        huntingRecord.likes.splice(likeIndex, 1)
+    }
+
+    await huntingRecord.save();
+    return huntingRecord;
+};
+
+commentSchema.methods.patchDislikes = async function(uid) {
+	const huntingRecord = this;
+    
+    let likeIndex = huntingRecord.likes.indexOf(uid);
+    if (likeIndex !== -1) {
+        huntingRecord.likes.splice(likeIndex, 1);
+    }
+
+    let dislikeIndex = huntingRecord.dislikes.indexOf(uid);
+    if ( dislikeIndex === -1) {
+        huntingRecord.dislikes.push(uid)
+    } else {
+        huntingRecord.dislikes.splice(likeIndex, 1)
+    }
+
+    await huntingRecord.save();
+    return huntingRecord;
+};
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
